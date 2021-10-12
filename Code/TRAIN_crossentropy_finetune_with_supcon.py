5c1ccf5421f0e74fbe4889cdfa83750426bb90df
jhyukjang <jhyuk@kaist.ac.kr>
2021-10-12 10:40:42 +0900

    code update

---
+++ Code/TRAIN_crossentropy_finetune_with_supcon.py
@@ -0,0 +1,166 @@
+import torch
+import torchvision.models as models
+from torchvision.datasets import ImageFolder
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import transforms, utils
+import os
+import pdb
+import numpy as np
+import matplotlib.pyplot as plt
+from pytorch_metric_learning import losses
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+from sklearn.metrics import jaccard_score
+import warnings
+from itertools import permutations
+
+warnings.simplefilter("ignore", UserWarning)
+
+
+activation = {}
+def get_activation(name):
+    def hook(model, input, output):
+        activation[name] = output.detach()
+    return hook
+
+def remap_labels(pred_labels, true_labels):
+    """Rename prediction labels (clustered output) to best match true labels."""
+    # from itertools import permutations # import this into script.
+    pred_labels, true_labels = np.array(pred_labels), np.array(true_labels)
+    assert pred_labels.ndim == 1 == true_labels.ndim
+    assert len(pred_labels) == len(true_labels)
+    cluster_names = np.unique(pred_labels)
+    accuracy = 0
+
+    perms = np.array(list(permutations(np.unique(true_labels))))
+
+    remapped_labels = true_labels
+    for perm in perms:
+        flipped_labels = np.zeros(len(true_labels))
+        for label_index, label in enumerate(cluster_names):
+            flipped_labels[pred_labels == label] = perm[label_index]
+
+        testAcc = np.sum(flipped_labels == true_labels) / len(true_labels)
+        if testAcc > accuracy:
+            accuracy = testAcc
+            remapped_labels = flipped_labels
+
+    return accuracy, remapped_labels
+
+def train(epoch, model, device, train_loader, optimizer, criterion_c):
+    results = {
+        'loss_c': 0
+    }
+    model.train()
+    total_train = len(train_loader)
+    result = []
+    label = []
+    for i, (x, y) in enumerate(train_loader):
+        x = x.to(device)
+        y = y.to(device)  # (batch_size, height, width)
+        label.append(y)
+        output = model(x).squeeze()
+        result.append(output)
+         # output = output.permute(0,2,3,1).contiguous().view(-1,5)
+        # y = y.unsqueeze(1).permute(0,2,3,1).contiguous().view(-1)
+        loss_c = criterion_c(output, y)
+        optimizer.zero_grad()
+        loss_c.backward()
+        optimizer.step()
+        # if i % train_log_step == 0:
+        #     print('Epoch [{}/{}], Step [{}/{}], Loss_C: {:.4f}'.format(epoch, 100, i, total_train, loss_c.item()))
+        results['loss_c'] += loss_c.item()
+    results['loss_c'] /= total_train
+
+    return results
+
+def validate(model, device, test_loader, criterion_c):
+    results = {
+        'loss_v': 0
+    }
+    model.eval()
+    total_valid = len(test_loader)
+    result = []
+    label = []
+    with torch.no_grad():
+        for k, (x, y) in enumerate(test_loader):
+            x = x.to(device)
+            y = y.to(device)
+            label.append(y)
+            output = model(x).squeeze()
+            loss_v = criterion_c(output, y)
+            result.append(output)
+
+            results['loss_v'] += loss_v.item()
+    results['loss_v'] /= total_valid
+
+    features = torch.cat(result)
+    features = features.cpu().numpy()
+    labels = torch.cat(label)
+    labels = labels.cpu().numpy()
+
+    cdict = {0: 'red', 1: 'blue', 2: 'green', 3: 'violet'}
+
+    # TSNE
+    tsne = TSNE(learning_rate=100, init='pca')
+    transformed = tsne.fit_transform(features)
+    xs = transformed[:, 0]
+    ys = transformed[:, 1]
+
+    kmeans = KMeans(n_clusters=4, random_state=0).fit(features)
+
+    remapped_labels = remap_labels(kmeans.labels_, labels)[1]
+    score = jaccard_score(labels, remapped_labels, average=None).mean()
+
+    return results, score
+
+transform = transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor()])
+
+train_dataset = ImageFolder(root='../Dataset/train_few/',transform=transform)
+val_dataset = ImageFolder(root='../Dataset/val/',transform=transform)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=84, shuffle=True, num_workers=1)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=84, shuffle=True, num_workers=1)
+
+n_classes = 3
+
+resnet50 = models.resnet50(pretrained=True)
+# for param in resnet50.parameters():
+#     param.requires_grad = False
+resnet50.fc = nn.Linear(2048,n_classes)
+
+
+resnet50 = torch.nn.Sequential(*(list(resnet50.children())[:-1]))
+
+backbone_params = [param for name, param in resnet50.named_parameters() if 'fc' not in name]
+other_params = [param for name, param in resnet50.named_parameters() if 'fc' in name]
+
+resnet50.cuda()
+# criterion = nn.CrossEntropyLoss()
+criterion = losses.SupConLoss(temperature=1)
+device = 'cuda'
+
+
+optimizer = optim.SGD([
+    {"params":backbone_params, 'lr':3e-3},
+    {"params":other_params, 'lr': 3e-2}],momentum=0.9)
+
+
+loss_v = 10
+score_c = 0.1
+
+for epoch in range(1):
+    print('Beginning Epoch {:02d}'.format(epoch))
+    train_results = train(epoch, resnet50, device, train_loader, optimizer, criterion)
+    print('TRAIN : LOSS_C: {:.4f} '.format(train_results['loss_c']))
+    val_results, val_clustering_score = validate(resnet50, device, val_loader,  criterion)
+    print('TEST : LOSS_V: {:.4f} '.format(val_results['loss_v']) + f'SCORE: {val_clustering_score}')
+    if score_c < val_clustering_score:
+        torch.save(resnet50.state_dict(), os.path.join('./crossentropy_weight/', 'maximum_clustering_finetuning2.pth'))
+        score_c = val_clustering_score
+        if score_c == 1.0:
+            break
+    if loss_v > val_results['loss_v']:
+        torch.save(resnet50.state_dict(), os.path.join('./crossentropy_weight/', 'minimum_loss_finetuning2.pth'))
+        loss_v = val_results['loss_v']
---
+++ Code/VISUALIZE_crossentropy_finetune_with_supcon_NMI.py
@@ -0,0 +1,168 @@
+import torch
+import torchvision.models as models
+from torchvision.datasets import ImageFolder
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import transforms, utils
+import os
+import pdb
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+
+from sklearn.neighbors import KNeighborsClassifier
+from torch.utils.data import TensorDataset
+import random
+from sklearn.metrics import jaccard_score, silhouette_samples, silhouette_score
+from itertools import permutations
+
+def remap_labels(pred_labels, true_labels):
+    """Rename prediction labels (clustered output) to best match true labels."""
+    # from itertools import permutations # import this into script.
+    pred_labels, true_labels = np.array(pred_labels), np.array(true_labels)
+    assert pred_labels.ndim == 1 == true_labels.ndim
+    assert len(pred_labels) == len(true_labels)
+    cluster_names = np.unique(pred_labels)
+    accuracy = 0
+
+    perms = np.array(list(permutations(np.unique(true_labels))))
+
+    remapped_labels = true_labels
+    for perm in perms:
+        flipped_labels = np.zeros(len(true_labels))
+        for label_index, label in enumerate(cluster_names):
+            flipped_labels[pred_labels == label] = perm[label_index]
+
+        testAcc = np.sum(flipped_labels == true_labels) / len(true_labels)
+        if testAcc > accuracy:
+            accuracy = testAcc
+            remapped_labels = flipped_labels
+
+    return accuracy, remapped_labels
+
+transform = transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor()])
+
+test_dataset = ImageFolder(root='../Dataset/test/',transform=transform)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False, num_workers=1)
+
+n_classes = 3
+
+resnet50 = models.resnet50(pretrained=False)
+
+resnet50.fc = nn.Linear(2048,n_classes)
+
+resnet50 = torch.nn.Sequential(*(list(resnet50.children())[:-1]))
+
+resnet50.cuda()
+# resnet50.load_state_dict(torch.load(f'./crossentropy_weight/minimum_loss.pth'))
+resnet50.load_state_dict(torch.load(f'./crossentropy_weight/maximum_clustering_finetuning2.pth'))
+device = 'cuda'
+
+#feature extract
+def get_activation(name):
+    def hook(model, input, output):
+        activation[name] = output.detach()
+    return hook
+
+activation = {}
+# resnet50.fc.register_forward_hook(get_activation('fc'))
+resnet50.eval()
+
+result = []
+label = []
+with torch.no_grad():
+    for k, (x, y) in enumerate(test_loader):
+        x = x.to(device)
+        y = y.to(device)
+        label.append(y)
+        output = resnet50(x).squeeze()
+        result.append(output)
+        _,pred = torch.max(output,1)
+
+features = torch.cat(result)
+features = features.cpu().numpy()
+labels = torch.cat(label)
+labels = labels.cpu().numpy()
+
+cdict = {0: 'red', 1: 'blue', 2: 'green', 3: 'violet'}
+
+
+#TSNE
+tsne = TSNE(learning_rate=100, init='pca', random_state=1024)
+transformed = tsne.fit_transform(features)
+xs = transformed[:,0]
+ys = transformed[:,1]
+
+fig, ax = plt.subplots()
+
+for g in np.unique(labels):
+    ix = np.where(labels == g)
+    ax.scatter(xs[ix], ys[ix], c = cdict[g], label = g)
+ax.legend()
+
+ax.axes.xaxis.set_visible(False)
+ax.axes.yaxis.set_visible(False)
+
+plt.title('Ground Truth')
+plt.tight_layout()
+plt.savefig('./pictures/after_GT')
+plt.show()
+
+#kmeans
+
+kmeans = KMeans(n_clusters=4, random_state=0).fit(features)
+
+remapped_labels = remap_labels(kmeans.labels_,labels)[1].astype(int)
+
+fig, ax2 = plt.subplots()
+for g in np.unique(remapped_labels):
+    # pdb.set_trace()
+    ix = np.where(remapped_labels == g)
+    ax2.scatter(xs[ix], ys[ix], c = cdict[g], label = g)
+
+ax2.axes.xaxis.set_visible(False)
+ax2.axes.yaxis.set_visible(False)
+
+# plt.scatter(xs,ys,c = kmeans.labels_)
+ax2.legend()
+plt.title('K-Means Clustering')
+plt.tight_layout()
+plt.savefig('./pictures/after_KMeans')
+plt.show()
+
+print(jaccard_score(labels,remapped_labels,average=None))
+print(f'Average : {np.mean(jaccard_score(labels,remapped_labels,average=None))}')
+# print(jaccard_score(labels,dbscan.labels_,average=None))
+
+print(f'NMI score : {metrics.normalized_mutual_info_score(labels,remapped_labels)}')
+
+
+# remapped_labels  = Predicted labels
+# labels           = True labels
+
+# binart recall check
+from sklearn.metrics import recall_score
+# recall = recall_score(labels, remapped_labels)
+
+def convert_binary(array, idx):
+    arr = array.copy()
+    indices = (arr==idx)
+    arr[:]=0
+    arr[indices] = 1
+    return arr
+
+binary_recall_abnormal = recall_score(convert_binary(labels,0),convert_binary(remapped_labels,0),pos_label=0) * 100
+
+
+# clustering purity check
+clustering_purity_average = 100*((labels == remapped_labels).sum())/len(remapped_labels)
+
+clustering_purity_normal = 100*((labels[np.where(labels==0)]  == remapped_labels[np.where(labels==0)]).sum())/len(remapped_labels[np.where(labels==0)])
+clustering_purity_fire = 100*((labels[np.where(labels==1)]  == remapped_labels[np.where(labels==1)]).sum())/len(remapped_labels[np.where(labels==1)])
+clustering_purity_dark = 100*((labels[np.where(labels==2)]  == remapped_labels[np.where(labels==2)]).sum())/len(remapped_labels[np.where(labels==2)])
+clustering_purity_dust = 100*((labels[np.where(labels==3)]  == remapped_labels[np.where(labels==3)]).sum())/len(remapped_labels[np.where(labels==3)])
+pdb.set_trace()
